9acf7eaf5a3f944b74b7e75da3e2faf972c38109
vicblab <47749352+vicblab@users.noreply.github.com>
2019-02-18 15:56:19 +0100

    Add files via upload

---
+++ Hanoi/torres-de-hanoi/Torres_de_Hanoi/Disco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Disco
+    {
+        /*TODO: Decidir tipo de Valor
+        public int Valor { get; set; }
+        public String Valor { get; set; }
+        */
+        public int Valor { get; set; }
+
+
+    }
+}
